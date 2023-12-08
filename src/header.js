8651d03ccecd39fd712c2616c577c3670f95218a
Le-Vincent56 <le.vincent56@gmail.com>
2023-12-07 22:45:32 -0500

    Updated portfolio

--- src/header.js
+++ src/header.js
@@ -7,6 +7,7 @@ let handleHeader = (height = 0) => {
     const navigation = document.querySelector(".navigation");
     const header = document.querySelector(".header");
     const headerNav = document.querySelector(".header-nav");
+    const headerList = document.querySelector("#large-header");
 
     if(height == 0) {
         height = header.offsetHeight;
@@ -16,11 +17,13 @@ let handleHeader = (height = 0) => {
     const scrollThreshold = height / 2;
 
     if (scrollY > height) {
+        headerList.innerHTML = `<li><a href="#about">ABOUT</a></li><li><a href="#skills">SKILLS</a></li><li><a href="#projects-head">PROJECTS</a></li>`;
         navigation.style.top = "0"; // Bring the navigation menu down
         header.classList.add("scrolled");
         header.style.boxShadow = "0 2px 4px rgba(0, 0, 0, 0.3)";
         headerNav.classList.remove("invisible");
     } else {
+        headerList.innerHTML = "";
         header.classList.remove("scrolled");
         navigation.style.top = `${header.offsetHeight - scrollY - 1}px`; // Hide the navigation menu by adjusting top
         header.style.boxShadow = "none";
